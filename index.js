8bc7672ec7e0d5618c531e980bba6a5a5ea42bb9
emonShimoul <emonshimoul@gmail.com>
2025-04-23 11:24:11 +0600

    Make User Admin and install JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
